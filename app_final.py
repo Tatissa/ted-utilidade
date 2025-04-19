7239228996e2794f06086bfb8318f4b9b615aa21
Tatissa <tatissalombardi@hotmail.com>
2025-04-19 14:55:56 -0300

    Add files via upload

---
+++ app_final.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="TED Utilidade", layout="wide")
+
+st.title("TED Utilidade - Web App Cloud 🚀")
+st.write("Bem-vinda, Tatissa! Seu painel completo TED está carregado aqui.")
